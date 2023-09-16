882b488201e04e8398c33140f68c28fc755811e5
cabecrandall <cabecrandall@gmail.com>
2023-09-15 23:47:00 -0600

    Get Automator to AppEEARS

--- AppEEARs_Automator.py
+++ AppEEARs_Automator.py
@@ -3,20 +3,24 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+username = input('Enter Username:')
+password = input('Enter Password:')
 
 driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
 
-driver.get('http://www.google.com/')
+driver.get('https://urs.earthdata.nasa.gov/oauth/authorize?client_id=ZAQpxSrQNpk342OR77kisA&response_type=code&redirect_uri=https://appeears.earthdatacloud.nasa.gov/login&state=/task/area')
 
-time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element(By.ID, 'username')
 
-search_box = driver.find_ele
+search_box.send_keys(username)
 
-search_box.send_keys('ChromeDriver')
+search_box = driver.find_element(By.ID, 'password')
+
+search_box.send_keys(password)
 
 search_box.submit()
 
-time.sleep(5) # Let the user actually see something!
 
 driver.quit()
