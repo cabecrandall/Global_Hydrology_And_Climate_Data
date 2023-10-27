26dacde0cb2c861a0379d503ea1f7eb38c6f7aea
cabecrandall <cabecrandall@gmail.com>
2023-10-27 16:06:54 -0600

    BROKEN: almost completed processing for final compilation
    ET PET inator still broken

--- AppEEARS_StatCompressor.py
+++ AppEEARS_StatCompressor.py
@@ -1,5 +1,47 @@
 """
 This is a one-time-use tool to extract only the
-necessary data from the AppEEARS output csv's.
-Specifically,
+necessary data from the AppEEARS MOD16A2GF output csv's.
+Specifically, each file will be converted to two
+time series, one for ET and one for PET.
 """
+
+import os
+import pandas as pd
+from tqdm import tqdm
+
+def compress_ET_PET_Stats(folder, ET_output_folder, PET_output_folder):
+    # Create the output folders if they don't exist
+    if not os.path.exists(ET_output_folder):
+        os.makedirs(ET_output_folder)
+    if not os.path.exists(PET_output_folder):
+        os.makedirs(PET_output_folder)
+    loop = tqdm(total=len(os.listdir(folder)), position=0, leave=False)
+    # Iterate through the files in the folder
+    for file in os.listdir(folder):
+        # Only process the csv files
+
+        if file.endswith(".csv"):
+            try:
+                # Read the file
+                path = os.path.join(folder, file)
+                frame = pd.read_csv(path)
+                # Extract the ET and PET columns
+                ET_frame = frame[frame["Dataset"] == "ET_500m"]
+                PET_frame = frame[frame["Dataset"] == "PET_500m"]
+                ET_frame = ET_frame[["Date", "Mean"]]
+                PET_frame = PET_frame[["Date", "Mean"]]
+
+                # Write the ET and PET columns to their respective output folders
+                ET_path = os.path.join(ET_output_folder, "basin_" + file)
+                PET_path = os.path.join(PET_output_folder, "basin_" + file)
+                ET_frame.to_csv(ET_path, index=False)
+                PET_frame.to_csv(PET_path, index=False)
+            except:
+                print("Error processing file: " + file)
+        loop.update(1)
+
+def main():
+    compress_ET_PET_Stats("MOD16A2GF_ET_PET", "Basin_ET_TS", "Basin_PET_TS")
+
+if __name__ == "__main__":
+    main()
---
+++ ET_PET_inator.py
@@ -0,0 +1,45 @@
+"""
+This file converts 8-day time series to daily time series
+by dividing the 8-day value by 8 and filling in the
+with the divided value for each day in the 8-day period.
+However, the last period is only 5 days (or 6 on leap years),
+so the last period is divided by 5 (or 6) and filled in
+"""
+
+import os
+import pandas as pd
+
+from tqdm import tqdm
+
+def convert_8day_to_daily(folder, destination):
+    loop = tqdm(total=len(os.listdir(folder)), position=0, leave=False)
+    for file in os.listdir(folder):
+        if file.endswith(".csv"):
+            path = os.path.join(folder, file)
+            frame = pd.read_csv(path)
+            frame["Date"] = pd.to_datetime(frame["Date"])
+            frame = frame.set_index("Date")
+            # Extend the frame to have a row for each day
+            frame = frame.resample("D").asfreq()
+            frame = frame.ffill()
+            frame['week_number'] = frame.index.isocalendar().week
+            frame['week_number'] = frame['week_number'].mul(frame.index.isocalendar().year)
+            # Divide groups of 8 identical values by 8,
+            # and groups of 5 by 5 and 6 by 6.
+            # This is done by grouping by the index
+            # and dividing the groups by the length of the group
+            frame = frame.groupby(['week_number']).transform(lambda x: x / len(x))
+            frame = frame.drop(columns=['week_number'])
+            frame = frame.reset_index()
+            frame.to_csv(os.path.join(destination, file), index=False)
+        loop.update(1)
+
+
+def main():
+    convert_8day_to_daily("Basin_ET_TS", "Basin_ET_TS_for_model")
+    convert_8day_to_daily("Basin_PET_TS", "Basin_PET_TS_for_model")
+
+
+if __name__ == "__main__":
+    main()
+    exit(0)
