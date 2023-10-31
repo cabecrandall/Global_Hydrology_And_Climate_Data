98662e9672449f802e6ce1fa86dbfe531d358e41
cabecrandall <cabecrandall@gmail.com>
2023-10-31 10:02:21 -0600

    Finished Converter and filter

--- Final_Series_Converter.py
+++ Final_Series_Converter.py
@@ -12,25 +12,32 @@ def toLitersPerDay(directory, destination):
     for file in os.listdir(directory):
         if file.endswith(".csv"):
             ID = file[-11:-4]
-            catchment_area = metadata.loc[metadata['Catchment ID'] == ID, 'Catchment Area'].values[0]
+            catchment_area = metadata.loc[metadata['Catchment ID'] == int(ID), 'Catchment Area'].values
+            if len(catchment_area) == 0:
+                print("No catchment area found for ID: " + ID)
+                continue
+            else:
+                catchment_area = catchment_area[0]
             path = os.path.join(directory, file)
             frame = pd.read_csv(path)
             frame['Date'] = pd.to_datetime(frame['Date'])
             # frame['Date'] = frame['Date'].dt.strftime('%m/%d/%Y')
             frame.set_index('Date', inplace=True)
-            frame = frame.iloc[0].mul(86400000)
+            frame.iloc[:, 0] = frame.iloc[:, 0].mul(86400000)
 
             # optimized rain to liters per day
-            frame = frame.iloc[2].mul(catchment_area)
-            frame = frame.iloc[2].mul(1000000)
+            frame.iloc[:, 2] = frame.iloc[:, 2].mul(catchment_area)
+            frame.iloc[:, 2] = frame.iloc[:, 2].mul(1000000)
 
             # optimized ET to liters per day
-            frame = frame.iloc[3].mul(1000000)
-            frame = frame.iloc[3].mul(catchment_area)
+            frame.iloc[:, 3] = frame.iloc[:, 3].mul(1000000)
+            frame.iloc[:, 3] = frame.iloc[:, 3].mul(catchment_area)
 
             # optimized PET to liters per day
-            frame = frame.iloc[4].mul(1000000)
-            frame = frame.iloc[4].mul(catchment_area)
+            frame.iloc[:, 4] = frame.iloc[:, 4].mul(1000000)
+            frame.iloc[:, 4] = frame.iloc[:, 4].mul(catchment_area)
+
+            frame.rename(columns={'ET [kg/m^2/day]': 'ET', 'PET [kg/m^2/day]': 'PET'}, inplace=True)
 
             frame.to_csv(os.path.join(destination, file), index=True)
 
@@ -43,36 +50,41 @@ def toLitersPerDayPerSqKm(directory, destination):
     for file in os.listdir(directory):
         if file.endswith(".csv"):
             ID = file[-11:-4]
-            catchment_area = metadata.loc[metadata['Catchment ID'] == ID, 'Catchment Area'].values[0]
+            catchment_area = metadata.loc[metadata['Catchment ID'] == int(ID), 'Catchment Area'].values
+            if len(catchment_area) == 0:
+                print("No catchment area found for ID: " + ID)
+                continue
+            else:
+                catchment_area = catchment_area[0]
             path = os.path.join(directory, file)
             frame = pd.read_csv(path)
             frame['Date'] = pd.to_datetime(frame['Date'])
-            # frame['Date'] = frame['Date'].dt.strftime('%m/%d/%Y')
             frame.set_index('Date', inplace=True)
-            frame = frame.iloc[0].mul(86400000)
-            frame = frame.iloc[0].floordiv(catchment_area)
+            frame.iloc[:, 0] = frame.iloc[:, 0].mul(86400000)
+            frame.iloc[:, 0] = frame.iloc[:, 0].floordiv(catchment_area)
 
             # optimized rain to liters per day per sq km
-            frame = frame.iloc[2].mul(1000000)
+            frame.iloc[:, 2] = frame.iloc[:, 2].mul(1000000)
 
             # optimized ET to liters per day per sq km
-            frame = frame.iloc[3].mul(1000000)
+            frame.iloc[:, 3] = frame.iloc[:, 3].mul(1000000)
 
             # optimized PET to liters per day per sq km
-            frame = frame.iloc[4].mul(1000000)
+            frame.iloc[:, 4] = frame.iloc[:, 4].mul(1000000)
 
+            frame.rename(columns={'ET [kg/m^2/day]': 'ET', 'PET [kg/m^2/day]': 'PET'}, inplace=True)
             frame.to_csv(os.path.join(destination, file), index=True)
 
         loop.update(1)
 
 def main():
-    print(metadata['Catchment Area'])
-    print(metadata['Catchment ID'])
-    toLitersPerDay("FilledFinalSeries", "FilledFinalSeries_LitersPerDay")
+    # toLitersPerDay("FilledFinalSeries", "FilledFinalSeries_LitersPerDay")
     toLitersPerDayPerSqKm("FilledFinalSeries", "FilledFinalSeries_LitersPerDayPerSqKm")
+
 
 if __name__ == '__main__':
     main()
     exit(0)
+
 
 
---
+++ Final_Series_Filter.py
@@ -0,0 +1,30 @@
+"""
+Module to filter final time series down to only the rows
+that have data in every column. Nice and simple!
+"""
+import os
+import pandas as pd
+from tqdm import tqdm
+
+
+def filter_final_series(folder, destination):
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+    loop = tqdm(total=len(os.listdir(folder)), position=0, leave=False)
+    for file in os.listdir(folder):
+        if file.endswith(".csv"):
+            path = os.path.join(folder, file)
+            frame = pd.read_csv(path)
+            frame = frame.dropna()
+            frame.to_csv(os.path.join(destination, file), index=False)
+        loop.update(1)
+
+
+def main():
+    filter_final_series("FilledFinalSeries_unfiltered", "FilledFinalSeries")
+
+
+if __name__ == "__main__":
+    main()
+    exit(0)
+
